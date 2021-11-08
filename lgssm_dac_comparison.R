9c3d13703b548fa2d0e49e3558952b924e772146
Francesca Crucinio <francescacrucinio@Francescas-MacBook-Pro.local>
2021-11-08 15:46:07 +0000

    benchmark

--- lgssm_dac_comparison.R
+++ lgssm_dac_comparison.R
@@ -42,66 +42,8 @@ for (t in 1:Time.step){
 
 
 ### DAC
-Nparticles <- 1000
+Nparticles <- 100
 Nrep <- 10
-se <- rep(list(array(0, dim = c(Time.step, d, Nrep))), times = 3)
-vse <- rep(list(array(0, dim = c(Time.step, d, Nrep))), times = 3)
-Zrep <- matrix(0, nrow = Nrep, ncol = 3)
-trep <- matrix(0, nrow = Nrep, ncol = 3)
-
-for (j in 1:Nrep){
-  x0 <- mvrnorm(n = Nparticles, mu0, Sigma0)
-  # dac linear cost
-  tic()
-  res_dac <- dac_time_lgssm(tau, lambda, sigmaY, Nparticles, x0, y, method = "lc")
-  runtime <- toc()
-  trep[j, 1] <- runtime$toc - runtime$tic
-  Zrep[j, 1] <- sum(res_dac[, 2*d+1])
-  se[1][[1]][, , j] <- (res_dac[, 1:d] - t(true_means))^2
-  vse[1][[1]][, , j] <- (res_dac[, (d+1):(2*d)] - true_variances)^2
-
-  # dac with mixture weights
-  tic()
-  res_dac_mix <- dac_time_lgssm(tau, lambda, sigmaY, Nparticles, x0, y, method = "mix")
-  runtime <- toc()
-  trep[j, 2] <- runtime$toc - runtime$tic
-  Zrep[j, 2] <- sum(res_dac_mix[, 2*d+1])
-  se[2][[1]][, , j] <- (res_dac_mix[, 1:d] - t(true_means))^2
-  vse[2][[1]][, , j] <- (res_dac_mix[, (d+1):(2*d)] - true_variances)^2
-
-  # dac with lightweight mixture weighting
-  tic()
-  res_dac_light <- dac_time_lgssm(tau, lambda, sigmaY, Nparticles, x0, y, method = "light")
-  runtime <- toc()
-  trep[j, 3] <- runtime$toc - runtime$tic
-  Zrep[j, 3] <- sum(res_dac_light[, 2*d+1])
-  se[3][[1]][, , j] <- (res_dac_light[, 1:d] - t(true_means))^2
-  vse[3][[1]][, , j] <- (res_dac_light[, (d+1):(2*d)] - true_variances)^2
-}
-mse <- array(0, dim = c(Time.step, d, 3))
-vmse <- array(0, dim = c(Time.step, d, 3))
-for (j in 1:3) {
-  mse[, , j] <- apply(se[j][[1]], c(1,2), mean)
-  vmse[, , j] <- apply(vse[j][[1]], c(1,2), mean)
-}
-
-g <- as.factor(rep(c("dac", "mix", "light"), each = Time.step))
-# MSE
-df <- data.frame(x = rep(1:Time.step, times = 3), y = c(rowMeans(mse[, , 1]), rowMeans(mse[, , 2]), rowMeans(mse[, , 3])), g)
-ggplot(data = df, aes(x = x, y= y, group = g)) +
-  geom_line(aes(x = x, y= y, color = g), size = 2) +
-  scale_y_continuous(trans='log10') +
-  theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
-        legend.title = element_blank(), legend.text=element_text(size=15))
-# RMSE
-df <- data.frame(x = rep(1:Time.step, times = 3), y = c(rowMeans(mse[, , 1]/true_variances),
-                                                        rowMeans(mse[, , 2]/true_variances), rowMeans(mse[, , 3]/true_variances)), g)
-ggplot(data = df, aes(x = x, y= y, group = g)) +
-  geom_line(aes(x = x, y= y, color = g), size = 2) +
-  scale_y_continuous(trans='log10') +
-  theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
-        legend.title = element_blank(), legend.text=element_text(size=15))
-
 df_dac <- data.frame()
 df_dac_mix <- data.frame()
 df_dac_light <- data.frame()
@@ -133,5 +75,34 @@ res <- bench::mark("dac" = {
 },
 memory = capabilities("profmem"),
 check = FALSE,
-iterations = 9,
+iterations = Nrep,
 filter_gc = TRUE)
+
+mse <- rep(0, times = 3*Nrep)
+rmse <- rep(0, times = 3*Nrep)
+for (j in 1:Nrep) {
+  mse[j] <- rowMeans(df_dac[j*Time.step, 1:d])
+  rmse[j] <- rowMeans(df_dac[j*Time.step, 1:d]/true_variances)
+  mse[Nrep+j] <- rowMeans(df_dac_mix[j*Time.step, 1:d])
+  rmse[Nrep+j] <- rowMeans(df_dac_mix[j*Time.step, 1:d]/true_variances)
+  mse[2*Nrep+j] <- rowMeans(df_dac_light[j*Time.step, 1:d])
+  rmse[2*Nrep+j] <- rowMeans(df_dac_light[j*Time.step, 1:d]/true_variances)
+}
+
+g <- as.factor(rep(c("dac", "mix", "light"), each = Nrep))
+times <- rep(res$total_time/res$n_itr, each = Nrep)
+memory <- rep(res$mem_alloc/1e6, each = Nrep)
+# time
+df <- data.frame(x1 = times, x2 = memory, y = mse, g)
+ggplot(data = df, aes(x = x1, y = y, group = g, fill = g)) +
+  geom_boxplot(aes(x = x1, y= y), coef = 6) +
+  scale_y_continuous(trans='log10') +
+  scale_x_continuous(trans='log10') +
+  theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
+        legend.title = element_blank(), legend.text=element_text(size=15))
+# memory
+ggplot(data = df, aes(x = x2, y = y, group = g, fill = g)) +
+  geom_boxplot(aes(x = x2, y= y), coef = 6) +
+  scale_y_continuous(trans='log10') +
+  theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
+        legend.title = element_blank(), legend.text=element_text(size=15))
