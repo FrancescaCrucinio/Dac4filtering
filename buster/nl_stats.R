fab2da0d516911af98eb6516d7cd91f99dfb5674
Francesca <francesca.crucinio@warwick.ac.uk>
2022-04-25 10:30:51 +0100

    nl plot

--- buster/nl_stats.R
+++ buster/nl_stats.R
@@ -20,7 +20,7 @@ nsmc_time <- rep(0, times = 50)
 stpf_time <- rep(0, times = 50)
 for (i in 1:50){
   ground_truth <- unname(data.matrix(read.csv(paste0("/storage/u1693998/data/data_truth_nl_tau_", tau, "d", d, "ID", i),
-                                              row.names = 1, nrows=d, skip=(Time.step-1)*d)))
+                                              row.names = 1, nrows=d, skip=(Time.step)*d)))
   d1 <- read.csv(paste0("/storage/u1693998/results/results/stpf_nl_iid_d", d, "N", Nparticles, "ID", i), row.names = 1,
                  nrows=d, skip=(Time.step-1)*d)
   d2 <- read.csv(paste0("/storage/u1693998/results/results/nsmc_nl_iid_d", d, "N", Nparticles, "ID", i), row.names = 1,
@@ -35,18 +35,19 @@ for (i in 1:50){
   mse_stpf[, , i] <- (ground_truth - data_stpf[, , i])^2
 }
 
+
+var_nsmc <- apply(mse_nsmc, c(1,2), var)
+var_stpf <- apply(mse_stpf, c(1,2), var)
+
 mse_nsmc <- apply(mse_nsmc, c(1,2), mean)
 mse_stpf <- apply(mse_stpf, c(1,2), mean)
-
-var_nsmc <- apply(data_nsmc, c(1,2), var)
-var_stpf <- apply(data_stpf, c(1,2), var)
 
 df_nsmc <- data.frame(rbind(mse_nsmc, var_nsmc))
 df_nsmc$type <- rep(c("mse", "var"), each = d)
 df_nsmc$runtime <- mean(nsmc_time)
-write.csv(x=df_nsmc, file=paste0("run_dac/results/stats_nsmc_nl_iid_d", d, "N", Nparticles, "ID", ID))
+write.csv(x=df_nsmc, file=paste0("run_dac/results/stats_nsmc_nl_iid_d", d, "N", Nparticles))
 
 df_stpf <- data.frame(rbind(mse_stpf, var_stpf))
 df_stpf$type <- rep(c("mse", "var"), each = d)
 df_stpf$runtime <- mean(stpf_time)
-write.csv(x=df_stpf, file=paste0("run_dac/results/stats_stpf_nl_iid_d", d, "N", Nparticles, "ID", ID))
+write.csv(x=df_stpf, file=paste0("run_dac/results/stats_stpf_nl_iid_d", d, "N", Nparticles))
--- nl_plot.R
+++ nl_plot.R
@@ -13,7 +13,7 @@ Nparticles <- 100
 M <- 100
 
 # nsmc
-df_nsmc <- read.csv(paste0("data/nl/stats_nsmc_nl_iid_d", d, "N", Nparticles, "ID1"),
+df_nsmc <- read.csv(paste0("data/nl/stats_nsmc_nl_iid_d", d, "N", Nparticles),
                     row.names = 1)
 mse_nsmc <- df_nsmc[1:d, 1:d]
 colnames(mse_nsmc) <- paste0("c",seq(1,d))
@@ -35,7 +35,7 @@ df1var <- var_nsmc %>%
 df1var$algo <- "nsmc"
 
 # stpf
-df_stpf <- read.csv(paste0("data/nl/stats_stpf_nl_iid_d", d, "N", Nparticles, "ID1"),
+df_stpf <- read.csv(paste0("data/nl/stats_stpf_nl_iid_d", d, "N", Nparticles),
                     row.names = 1)
 mse_stpf <- df_stpf[1:d, 1:d]
 colnames(mse_stpf) <- paste0("c",seq(1,d))
@@ -56,25 +56,49 @@ df2var <- var_stpf %>%
   mutate(col = fct_relevel(col, colnames(var_stpf)))
 df2var$algo <- "stpf"
 
+# dac
+df_dac <- read.csv(paste0("data/nl/stats_dac_nl_iid_d", d, "N", Nparticles),
+                    row.names = 1)
+mse_dac <- df_dac[1:d, 1:d]
+colnames(mse_dac) <- paste0("c",seq(1,d))
+rownames(mse_dac) <- paste0("r",seq(1,d))
+df3 <- mse_dac %>%
+  as.data.frame() %>%
+  rownames_to_column("r_id") %>%
+  pivot_longer(-c(r_id), names_to = "col", values_to = "mse") %>%
+  mutate(col = fct_relevel(col, colnames(mse_dac)))
+df3$algo <- "dac"
+var_dac <- df_dac[(d+1):(2*d), 1:d]
+colnames(var_dac) <- paste0("c",seq(1,d))
+rownames(var_dac) <- paste0("r",seq(1,d))
+df3var <- var_dac %>%
+  as.data.frame() %>%
+  rownames_to_column("r_id") %>%
+  pivot_longer(-c(r_id), names_to = "col", values_to = "var") %>%
+  mutate(col = fct_relevel(col, colnames(var_dac)))
+df3var$algo <- "dac"
+
 # MSE
-df <- rbind(df1, df2)
+df <- rbind(df1, df2, df3)
 
 ggplot(data = df, aes(x = col , y = factor(r_id, level = paste0("r", d:1)), fill = mse)) +
 geom_raster() +
 scale_fill_viridis_c() +
-facet_wrap(~algo, ncol = 2, nrow = 1) +
+facet_wrap(~algo, ncol = 2, nrow = 2) +
 theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
         axis.text = element_blank(), axis.ticks = element_blank(),
         legend.title = element_blank(), legend.text=element_text(size=20),
-        text = element_text(size=15))
+        text = element_text(size=15), strip.text.x = element_text(size = 20))
+ggsave("nl_d32N100_mse.pdf", width = 12, height = 12, dpi = 300)
 
 # variance
-df_var <- rbind(df1var, df2var)
+df_var <- rbind(df1var, df2var, df3var)
 ggplot(data = df_var, aes(x = col , y = factor(r_id, level = paste0("r", d:1)), fill = var)) +
   geom_raster() +
   scale_fill_viridis_c() +
-  facet_wrap(~algo, ncol = 2, nrow = 1) +
+  facet_wrap(~algo, ncol = 2, nrow = 2) +
   theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
         axis.text = element_blank(), axis.ticks = element_blank(),
         legend.title = element_blank(), legend.text=element_text(size=20),
-        text = element_text(size=15))
+        text = element_text(size=15), strip.text.x = element_text(size = 20))
+ggsave("nl_d32N100_var.pdf", width = 12, height = 12, dpi = 300)
