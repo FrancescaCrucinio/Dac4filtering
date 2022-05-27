809e98f8855110b183a96b9d3baf9ecd9b0fdcf2
Francesca <francesca.crucinio@warwick.ac.uk>
2022-05-27 09:27:47 +0100

    cleaner plots

--- lgssm_distances_plot.R
+++ lgssm_distances_plot.R
@@ -1,111 +1,3 @@
-# Plot of W1 and KS distance for linear Gaussian SSM
-library(ggpubr)
-# read data
-df <- read.csv(paste0("data/lgssm_tempering/lgssm_d32N100ID1"))
-df$N <- "10^2"
-df$d <- "32"
-df$run <- 1
-dfnew <- read.csv(paste0("data/lgssm_tempering/marginal_lgssm_d32N100ID1"))
-dfnew$N <- "10^2"
-dfnew$d <- "32"
-dfnew$run <- 1
-df <- rbind(df, dfnew)
-dfnew <- read.csv(paste0("data/lgssm_tempering/lgssm_d256N100ID1"))
-dfnew$N <- "10^2"
-dfnew$d <- "256"
-dfnew$run <- 1
-df <- rbind(df, dfnew)
-dfnew <- read.csv(paste0("data/lgssm_tempering/marginal_lgssm_d256N100ID1"))
-dfnew$N <- "10^2"
-dfnew$d <- "256"
-dfnew$run <- 1
-df <- rbind(df, dfnew)
-dfnew <- read.csv(paste0("data/lgssm_tempering/lgssm_d2048N100ID1"))
-dfnew$N <- "10^2"
-dfnew$d <- "2048"
-dfnew$run <- 1
-df <- rbind(df, dfnew)
-dfnew <- read.csv(paste0("data/lgssm_tempering/marginal_lgssm_d2048N100ID1"))
-dfnew$N <- "10^2"
-dfnew$d <- "2048"
-dfnew$run <- 1
-df <- rbind(df, dfnew)
-for (id in 2:50){
-  filename <- paste0("data/lgssm_tempering/lgssm_d32N100ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^2"
-  dfnew$d <- "32"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  dfnew <- read.csv(paste0("data/lgssm_tempering/marginal_lgssm_d32N100ID", id, sep = ""))
-  dfnew$N <- "10^2"
-  dfnew$d <- "32"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  filename <- paste0("data/lgssm_tempering/lgssm_d256N100ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^2"
-  dfnew$d <- "256"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  filename <- paste0("data/lgssm_tempering/lgssm_d2048N100ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^2"
-  dfnew$d <- "2048"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-}
-for (id in 1:50){
-  filename <- paste0("data/lgssm_tempering/lgssm_d32N1000ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^3"
-  dfnew$d <- "32"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  filename <- paste0("data/lgssm_tempering/marginal_lgssm_d32N1000ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^3"
-  dfnew$d <- "32"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  filename <- paste0("data/lgssm_tempering/lgssm_d256N1000ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^3"
-  dfnew$d <- "256"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  filename <- paste0("data/lgssm_tempering/lgssm_d2048N1000ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^3"
-  dfnew$d <- "2048"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-}
-for (id in 1:50){
-  filename <- paste0("data/lgssm_tempering/lgssm_d32N10000ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^4"
-  dfnew$d <- "32"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  dfnew <- read.csv(paste0("data/lgssm_tempering/marginal_lgssm_d32N10000ID", id, sep = ""))
-  dfnew$N <- "10^4"
-  dfnew$d <- "32"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-  filename <- paste0("data/lgssm_tempering/lgssm_d256N10000ID", id, sep = "")
-  dfnew <- read.csv(filename)
-  dfnew$N <- "10^4"
-  dfnew$d <- "256"
-  dfnew$run <- id
-  df <- rbind(df, dfnew)
-}
-df <- df[, -1]
-df <- df[df$algo != "dac_ada", ]
-Time.step <- ncol(df) - 7
-colnames(df)[(Time.step+1):(Time.step+3)] <- c("w1", "ks", "runtime")
-
-
 # distances
 distances <- data.frame(aggregate(w1 ~ algo + runtime + N + run + d, data = df, FUN = "mean"))
 distances$ks <- aggregate(ks ~ algo + runtime + N + run + d, data = df, FUN = "mean")$ks
@@ -135,7 +27,7 @@ ggplot(data = distances, aes(x = runtime_mean, y = w1, group = interaction(algo,
         axis.title.y=element_blank(), strip.text.x = element_blank(),
         legend.title = element_blank(), legend.text=element_text(size=30),
         text = element_text(size=30), legend.position="none", panel.spacing = unit(2, "lines"))
-# ggsave("marginal_lgssm_w1.pdf", width = 20, height = 30, dpi = 300)
+# ggsave("marginal_lgssm_w1.pdf", width = 20, height = 35, dpi = 300)
 # Kolmogorov-Smirnov
 ggplot(data = distances, aes(x = runtime_mean, y = ks, group = interaction(algo, N, d), fill = algo, colour = algo)) +
   geom_boxplot(coef = 10, width = 0.1, alpha = 0.1, lwd = 1) +
@@ -155,4 +47,26 @@ ggplot(data = distances, aes(x = runtime_mean, y = ks, group = interaction(algo,
         axis.title.y=element_blank(), strip.text.x = element_blank(),
         legend.title = element_blank(), legend.text=element_text(size=30),
         text = element_text(size=30), legend.position="none", panel.spacing = unit(2, "lines"))
-# ggsave("marginal_lgssm_ks.pdf", width = 20, height = 30, dpi = 300)
+# ggsave("marginal_lgssm_ks.pdf", width = 20, height = 35, dpi = 300)
+
+p <- ggplot(data = distances, aes(x = runtime_mean, y = ks, group = interaction(algo, N, d), fill = algo, colour = algo)) +
+  geom_boxplot(coef = 10, width = 0.1, alpha = 0.1, lwd = 1) +
+  geom_point(data = distances_mean, shape = 4, lwd = 1, aes(x = runtime, y = ks, group = interaction(algo, N, d), fill = algo, colour = algo)) +
+  scale_x_log10(
+    breaks = scales::trans_breaks("log10", function(x) 10^x),
+    labels = scales::trans_format("log10", scales::math_format(10^.x))
+  ) +
+  scale_y_log10(
+    breaks = scales::trans_breaks("log10", function(x) 10^x),
+    labels = scales::trans_format("log10", scales::math_format(10^.x))
+  ) +
+  facet_wrap(~factor(d, levels = c("32", "256", "2048")), ncol = 1, nrow = 3) +
+  scale_fill_manual(values=c("#F8766D", "#E69F00", "#00BA38", "#619CFF")) +
+  scale_color_manual(values=c("#F8766D", "#E69F00", "#00BA38", "#619CFF")) +
+  theme(axis.title.x=element_blank(), axis.text = element_text(size=30),
+        axis.title.y=element_blank(), strip.text.x = element_blank(),
+        legend.title = element_blank(), legend.text=element_text(size=30),
+        text = element_text(size=30), legend.position="bottom", panel.spacing = unit(2, "lines"))
+my_legend <- get_legend(p)
+legend_p <- as_ggplot(my_legend)
+# ggsave("lgssm_legend.pdf", width = 8, height = 1, dpi = 300)
--- lgssm_rmse_plot.R
+++ lgssm_rmse_plot.R
@@ -130,18 +130,25 @@ for (id in 1:50){
   dfnew$algo <- "dac-ada"
   dfnew$run <- id
   df <- rbind(df, dfnew)
-  # filename <- paste0("data/lgssm_tempering/lgssm_d256N1000ID", id, sep = "")
-  # dfnew <- read.csv(filename)
-  # dfnew$N <- "10^3"
-  # dfnew$d <- "256"
-  # dfnew$run <- id
-  # df <- rbind(df, dfnew)
+  filename <- paste0("data/lgssm_tempering/lgssm_d256N1000ID", id, sep = "")
+  dfnew <- read.csv(filename)
+  dfnew$N <- "10^3"
+  dfnew$d <- "256"
+  dfnew$run <- id
+  df <- rbind(df, dfnew)
   # filename <- paste0("data/lgssm_tempering/marginal_lgssm_d256N1000ID", id, sep = "")
   # dfnew <- read.csv(filename)
   # dfnew$N <- "10^3"
   # dfnew$d <- "256"
   # dfnew$run <- id
   # df <- rbind(df, dfnew)
+  filename <- paste0("data/lgssm_marginal/adaptive_marginal_lgssm_d256N1000ID", id, sep = "")
+  dfnew <- read.csv(filename)
+  dfnew$N <- "10^3"
+  dfnew$d <- "256"
+  dfnew$algo <- "dac-ada"
+  dfnew$run <- id
+  df <- rbind(df, dfnew)
   # filename <- paste0("data/lgssm_tempering/lgssm_d2048N1000ID", id, sep = "")
   # dfnew <- read.csv(filename)
   # dfnew$N <- "10^3"
@@ -193,7 +200,7 @@ Time.step <- ncol(df) - 7
 colnames(df)[(Time.step+1):(Time.step+3)] <- c("w1", "ks", "runtime")
 
 tmp <- aggregate(. ~ algo + N + d, data = df, FUN = "mean")
-rmse_data <- data.frame(rep(1:Time.step, times = 15), rep(tmp$algo, each = 100), rep(tmp$N, each = 100), rep(tmp$d, each = 100))
+rmse_data <- data.frame(rep(1:Time.step, times = 18), rep(tmp$algo, each = 100), rep(tmp$N, each = 100), rep(tmp$d, each = 100))
 colnames(rmse_data) <- c("Time.step", "algo", "N", "d")
 rmse_data <- rmse_data[order(rmse_data$algo, rmse_data$N, rmse_data$d), ]
 tmp <- tmp[order(tmp$algo, tmp$N, tmp$d), ]
@@ -201,7 +208,8 @@ rmse_data$rmse <- as.vector(t(as.matrix(tmp[, 4:103])))
 ggplot(data = rmse_data, aes(x = Time.step, y = rmse, group = algo, colour = algo)) +
   geom_line(size = 2) +
   scale_y_continuous(trans='log10') +
-  facet_wrap(~factor(interaction(N, d), levels=c("10^2.32", "10^3.32", "10^4.32", "10^2.256", "10^3.256", "10^4.256", "10^2.2048", "10^3.2048")), ncol = 3, nrow = 3) +
+  facet_wrap(~factor(interaction(N, d), levels=c("10^2.32", "10^3.32", "10^2.256", "10^3.256", "10^2.2048", "10^3.2048")), ncol = 2, nrow = 3) +
+  # facet_wrap(~factor(interaction(N, d), levels=c("10^2.32", "10^3.32", "10^4.32", "10^2.256", "10^3.256", "10^4.256", "10^2.2048", "10^3.2048")), ncol = 3, nrow = 3) +
   scale_color_manual(values=c("#F8766D", "#E69F00", "#00BA38", "#619CFF")) +
   theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
         axis.text = element_text(size=20),
