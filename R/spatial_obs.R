2b3f67bb3f7293b63d1986e91e5613c37f2a1219
Francesca <francesca.crucinio@warwick.ac.uk>
2022-05-31 09:48:13 +0100

    corrected spatial obs

--- R/spatial_obs.R
+++ R/spatial_obs.R
@@ -3,6 +3,8 @@ spatial_obs <- function(d, sigmaX, nu, tau, tau_diag, Time.step){
   y.error.prec <- matrix(0, nrow = d^2, ncol = d^2)
   diag(y.error.prec) <- tau_diag
   diag(y.error.prec[-1, ]) <- tau
+  vertical_neighbours <- ((0:d^2) * (d^2 + 1) + d+1)
+  y.error.prec[vertical_neighbours[(vertical_neighbours <= d^4)]] <- tau
   y.error.prec[upper.tri(y.error.prec)] <- t(y.error.prec)[upper.tri(y.error.prec)]
   y.error.var <- inv(y.error.prec)
   y.error.var[upper.tri(y.error.var)] <- t(y.error.var)[upper.tri(y.error.var)]
@@ -13,6 +15,7 @@ spatial_obs <- function(d, sigmaX, nu, tau, tau_diag, Time.step){
 
   for (t in 2:(Time.step+1)) {
     x[, , t] <- x[, , t-1] + sqrt(sigmaX)*matrix(rnorm(d^2), nrow = d, ncol = d)
+    tmp = rmvt(1, mu = c(x[, , t]), S = y.error.var, df = nu)
     y[, , t-1] <- matrix(rmvt(1, mu = c(x[, , t]), S = y.error.var, df = nu), ncol = d)
   }
 
