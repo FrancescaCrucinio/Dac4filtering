78ae1fb1772351416dd65161ca9e6a9c5955dc33
Francesca <francesca.crucinio@warwick.ac.uk>
2022-07-01 08:30:24 +0100

    comparison bpf

--- spatial_comparison_plot.R
+++ spatial_comparison_plot.R
@@ -18,15 +18,15 @@ for (Nparticles in c(100, 500, 1000, 5000, 10000)) {
 }
 df_bpf <- read.csv(paste0("data/spatial/new_stats_bpf_spatial_tau", -tau, "d", d, "N", 100000),
                    row.names = 1)
-dim <- 4
+dim <- 3
 t <- 10
 df_plot <- df[df$t == t & df$dim == dim,]
 df_bpf_plot <- df_bpf[df_bpf$t == t & df_bpf$dim == dim,]
 ggplot(data = df_plot, aes(x=N, y=mean, group = N))+
   geom_boxplot(coef = 10, width = 0.1, alpha = 0.3, lwd = 1, color = "dark gray", fill = "dark gray") +
   geom_hline(yintercept = mean(df_bpf_plot$mean), lwd = 1, linetype = "dashed") +
-  geom_hline(yintercept = min(df_bpf_plot$mean), lwd = 1, linetype = "dashed") +
-  geom_hline(yintercept = max(df_bpf_plot$mean), lwd = 1, linetype = "dashed") +
+  geom_hline(yintercept = quantile(df_bpf_plot$mean, 0.25), lwd = 1, linetype = "dashed") +
+  geom_hline(yintercept = quantile(df_bpf_plot$mean, 0.75), lwd = 1, linetype = "dashed") +
     scale_x_log10(
     breaks = scales::trans_breaks("log10", function(x) 10^x),
     labels = scales::trans_format("log10", scales::math_format(10^.x))
@@ -34,4 +34,4 @@ ggplot(data = df_plot, aes(x=N, y=mean, group = N))+
   theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
         legend.title = element_blank(), legend.text=element_text(size=30),
         text = element_text(size=30))
-# ggsave("spatial2_boxplot_node22_mean.pdf", width = 8.5, height = 6, dpi = 300)
+# ggsave("spatial2_boxplot_node12_mean.pdf", width = 8.5, height = 6, dpi = 300)
